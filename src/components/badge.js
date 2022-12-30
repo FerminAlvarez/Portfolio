a11f1956c6548420fa55acfadd100acd4eb04636
Fermin Alvarez <feeralvarez013@gmail.com>
2022-12-30 14:41:44 -0300

    Fixed align icons projects

--- src/components/badge.js
+++ src/components/badge.js
@@ -3,9 +3,9 @@ import mapIcons from "../data/mapIcons";
 export default function Badge(props) {
   let color = mapIcons.get(props.tag);
   return (
-    <div className="title-hero text-sm flex flex-col items-center fill-gray my-1">
+    <div className="title-hero text-sm flex flex-col items-center fill-gray my-1 w-[70px]">
       {color}
-      <p className="max-w-[90px]">{props.tag}</p>
+      <p>{props.tag}</p>
     </div>
   );
 }
