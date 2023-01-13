8b5ed22e49f85a7e07dca66903e785606ce2d552
Fermin Alvarez <feeralvarez013@gmail.com>
2023-01-13 16:53:50 -0300

    Added cv

--- src/components/downloadcv.js
+++ src/components/downloadcv.js
@@ -3,7 +3,7 @@ export default function DownloadCV() {
   return (
     <div className="bg-secondary" >
       <a className="anchor invisible" id="cv"> </a>
-      <a href="" className="btn btn-lg my-20 transform transition duration-300 hover:scale-125 hover:bg-primary bg-primary text-black border-0">
+      <a href={process.env.PUBLIC_URL + "/cv/FerminAlvarez-CV.pdf"} download target="_blank" className="btn btn-lg my-20 transform transition duration-300 hover:scale-125 hover:bg-primary bg-primary text-black border-0">
         Descargar CV
         <span className="text-xl ml-2">
           <BsFileEarmarkText />
