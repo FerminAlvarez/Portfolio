6c3e6a9cdd4767c9d7a32fbef5da13e161b90b73
Fermin Alvarez <feeralvarez013@gmail.com>
2023-01-13 16:35:56 -0300

    Added career info

--- src/components/career.js
+++ src/components/career.js
@@ -3,10 +3,26 @@ import { IoMdCheckmark } from "react-icons/io";
 export default function Career() {
   return (
     <div className="flex flex-col text-black">
-      <h1 className="text-4xl sm:text-6xl font-bold title-hero mt-10">
+      <h1 className="text-4xl sm:text-6xl font-bold title-hero  mt-10">
         Experiencia Laboral
       </h1>
-      <div className="flex mx-auto my-10">
+      <div className="flex mx-auto mt-10 ">
+        <div className="hidden md:flex md:flex-col items-center md:mr-4">
+          <div>
+            <div className="flex items-center justify-center w-10 h-10 border rounded-full border-black">
+              <IoMdCheckmark size={25} />
+            </div>
+          </div>
+          <div className="w-px h-full bg-black" />
+        </div>
+        <div className="pt-1 pb-8 md:w-[28rem] ">
+          <p className="mb-2 text-lg font-bold">ICEL Cursos y Capacitaciones</p>
+          <p className="">www.icel.com.ar</p>
+          <p className="">Autónomo - Integrador de Aplicaciones</p>
+          <p className="">(2022)</p>
+        </div>
+      </div>
+      <div className="flex mx-auto">
         <div className="hidden md:flex md:flex-col items-center md:mr-4">
           <div>
             <div className="flex items-center justify-center w-10 h-10 border rounded-full border-black ">
@@ -14,7 +30,7 @@ export default function Career() {
             </div>
           </div>
         </div>
-        <div className="pt-1 pb-8 ">
+        <div className="pt-1 pb-8 md:w-[28rem]">
           <p className="mb-2 text-lg font-bold">Anteojos Negros</p>
           <p className="">www.anteojosnegros.com</p>
           <p className="">Pasante - Frontend Trainee</p>
